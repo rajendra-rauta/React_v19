680a82cf5d0e52c95f30f61d86d2addd15921996
Rajendra <rajendrarauta979@gmail.com>
2024-11-25 21:10:44 +0530

    Import and export

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 // import App.css fr 
+import { NetflixSeries } from "./components/NetflixSeries";
+import { Footer } from "./components/NetflixSeries";
+
 export const App = () => {
   // const value = "Rajendra Rauta"
   return (
@@ -7,40 +10,9 @@ export const App = () => {
       <NetflixSeries />
       <NetflixSeries />
       <NetflixSeries />
+      <Footer/>
 
     </>
   );
 };
 
-const NetflixSeries = () => {
-  const name = "Rajendra Rauta"
-  const ph = "7846828248"
-
-
-
-  let  age = 25
-  // let canWatch = "Watch Now"
-  // if(age<=18) canWatch = "Not Avilable";
-
-  const canWatch = () => {
-    if(age>=18)  return "Watch Now" ;
-    return "Not avilable"; 
-  };
-
-  return (
-
-    <div>
-      <div>
-        <img src="img.jpg" height={300} width={400}></img>
-      </div>
-      <h2>Name {name} </h2>
-      <h3>{ph}</h3>
-      <h3>hello</h3>
-      <p></p>
-      {/* <button>{age >= 18 ? "Watch Now" : "Not Avilabe!"}</button> */}
-      {/* <button>{canWatch}</button> */}
-      <button>{canWatch()}</button>
-    </div>
-
-  )
-}
---
+++ src/components/NetflixSeries.jsx
@@ -0,0 +1,43 @@
+export const NetflixSeries = () => {
+    const name = "Rajendra Rauta"
+    const ph = "7846828248"
+  
+  
+  
+    let  age = 25
+    // let canWatch = "Watch Now"
+    // if(age<=18) canWatch = "Not Avilable";
+  
+    const canWatch = () => {
+      if(age>=18)  return "Watch Now" ;
+      return "Not avilable"; 
+    };
+  
+    return (
+  
+      <div>
+        <div>
+          <img src="img.jpg" height={300} width={400}></img>
+        </div>
+        <h2>Name {name} </h2>
+        <h3>{ph}</h3>
+        <h3>hello</h3>
+        <p></p>
+        {/* <button>{age >= 18 ? "Watch Now" : "Not Avilabe!"}</button> */}
+        {/* <button>{canWatch}</button> */}
+        <button>{canWatch()}</button>
+      </div>
+  
+    )
+  };
+
+  
+//   export default NetflixSeries;
+
+
+
+
+
+export const Footer = () =>{
+  return <p>Footer Part</p>
+}
