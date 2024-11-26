bc056f326b22646810fbb3a79196c3fcfb91bb07
Rajendra <rajendrarauta979@gmail.com>
2024-11-26 09:28:40 +0530

    looping

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 // import App.css fr 
-import { NetflixSeries } from "./components/NetflixSeries";
-import { Footer } from "./components/NetflixSeries";
+import { NetflixSeries} from "./components/NetflixSeries";
+
 
 export const App = () => {
   // const value = "Rajendra Rauta"
   return (
     <>
       <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <NetflixSeries />
-      <Footer/>
-
     </>
   );
 };
--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,43 +1,39 @@
+import seriesData from "../api/seriesData.json"
 export const NetflixSeries = () => {
-    const name = "Rajendra Rauta"
-    const ph = "7846828248"
-  
-  
-  
-    let  age = 25
-    // let canWatch = "Watch Now"
-    // if(age<=18) canWatch = "Not Avilable";
-  
-    const canWatch = () => {
-      if(age>=18)  return "Watch Now" ;
-      return "Not avilable"; 
-    };
-  
-    return (
-  
-      <div>
-        <div>
-          <img src="img.jpg" height={300} width={400}></img>
-        </div>
-        <h2>Name {name} </h2>
-        <h3>{ph}</h3>
-        <h3>hello</h3>
-        <p></p>
-        {/* <button>{age >= 18 ? "Watch Now" : "Not Avilabe!"}</button> */}
-        {/* <button>{canWatch}</button> */}
-        <button>{canWatch()}</button>
-      </div>
-  
-    )
-  };
-
-  
-//   export default NetflixSeries;
+  //   const name = "Rajendra Rauta"
+  //   const ph = "7846828248"
 
 
 
+  //   let age = 25
+  //   // let canWatch = "Watch Now"
+  //   // if(age<=18) canWatch = "Not Avilable";
+
+  //   const canWatch = () => {
+  //     if (age >= 18) return "Watch Now";
+  //     return "Not avilable";
+  //   };
+
+  return (
+    <ul>
+      {seriesData.map((curElem) => {
+        return (
+          <li key={curElem.id}>
+            <div>
+              <img src={curElem.img_url} height={300} width={400}></img>
+            </div>
+            <h2>Name:{curElem.name} </h2>
+            <h3>Rating:{curElem.rating}</h3>
+            <p>Summary :{curElem.description}</p>
+            <p>Genre: {curElem.genre}</p>
+            <p>Cast: {curElem.cast}</p>
+            <a href={curElem.watch_url} target="_blank">
+              <button>Watch Now</button></a>
+          </li>
+        );
+      })}
+    </ul>
+  )
+};
 
 
-export const Footer = () =>{
-  return <p>Footer Part</p>
-}
