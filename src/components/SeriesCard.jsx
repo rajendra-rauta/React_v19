3bb7b66a6a98ad5af9bbb4aec1617f7d080fab69
Rajendra <rajendrarauta979@gmail.com>
2024-11-30 21:41:12 +0530

    css

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -3,10 +3,11 @@ export const SeriesCard = ({data}) =>{
     // eslint-disable-next-line react/prop-types
     const {id,img_url,name,rating,description,cast,genre,watch_url} =data;
     return (
-        <li>
+        <li className="card">
           <div>
             <img src={img_url} height={300} width={400}></img>
           </div>
+          <div className="card-contain">
           <h2>Name:{name} </h2>
           <h3>Rating:{rating}</h3>
           <p>Summary :{description}</p>
@@ -14,6 +15,7 @@ export const SeriesCard = ({data}) =>{
           <p>Cast: {cast}</p>
           <a href={watch_url} target="_blank">
             <button>Watch Now</button></a>
+            </div>
         </li>
       );
 }
