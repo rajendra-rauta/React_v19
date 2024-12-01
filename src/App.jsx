887d0718c6d9c99eca8ab9c0adeebe8a1fa6c091
Rajendra <rajendrarauta979@gmail.com>
2024-12-01 20:54:25 +0530

    style component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { NetflixSeries } from "./components/NetflixSeries";
-import "./components/Netflix.css";
+// import "./components/Netflix.css";
+import styles from "./components/Netflix.module.css";
 
 
 export const App = () => {
   // const value = "Rajendra Rauta"
   return (
  
-      <section className="container">
-      <h1 className="card-heading">List of Best Netflix Series</h1>
+      <section >   
+      <h1 className={styles["card-heading"]}>List of Best Netflix Series</h1>
         <NetflixSeries />
       </section>
    
   );
 };
+
+
 
--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,41 +1,44 @@
+import styles from "./Netflix.module.css";
+import styled from 'styled-components';
 
-export const SeriesCard = ({data}) =>{
-    // eslint-disable-next-line react/prop-types
-    const {img_url,name,rating,description,cast,genre,watch_url} =data;
-
-
-    const btn_style = {
-      padding:"1.2rem 2.4rem",
-              border:"none",
-              fontSize:"1.6rem",
-              backgroundColor:`${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
-              color :"var(--btn-color)",
-              fontWeight: "bold",
-              cursor :"pointer",
-            
-    };
-
- const rating_class= rating >= 8.5 ? "super_hit" : "average";
+export const SeriesCard = ({ data }) => {
+  // eslint-disable-next-line react/prop-types
+  const { img_url, name, rating, description, cast, genre, watch_url } = data;
 
 
+  // const btn_style = {}
+    const Button = styled.button ({
+ 
+      padding: "1.2rem 2.4rem",
+    border: "none",
+    fontSize: "1.6rem",
+    backgroundColor: `${rating >= 8.5 ? "#7dcea0" : "#f7dc6f"}`,
+    color: "var(--btn-color)",
+    fontWeight: "bold",
+    cursor: "pointer",
 
+    })
     
-    return (
-        <li className="card">
-          <div>
-            <img src={img_url} height={300} width={400}></img>
-          </div>
-          <div className="card-contain">
-          <h2>Name:{name} </h2>
-          <h3>Rating:<span className={`rating ${rating_class}`}>{rating}</span></h3>
-          <p>Summary :{description}</p>
-          <p>Genre: {genre}</p>
-          <p>Cast: {cast}</p>
-          <a href={watch_url} target="_blank">
-            <button style={btn_style}>Watch Now</button></a>
-            </div>
-        </li>
-      );
+ 
+  const rating_class = rating >= 8.5 ? styles.super_hit : styles.average;
+
+  return (
+    <li className={styles.card}>
+      <div>
+        <img src={img_url} height={300} width={400}></img>
+      </div>
+      <div className={styles["card-content"]}>
+        <h2>Name:{name} </h2>
+        <h3>Rating:<span className={`${styles.rating} ${rating_class}`}>{rating}</span></h3>
+        <p>Summary :{description}</p>
+        <p>Genre: {genre}</p>
+        <p>Cast: {cast}</p>
+        <a href={watch_url} target="_blank">
+          {/* <button style={btn_style}>Watch Now</button></a> */}
+         <Button> Watch Now</Button> </a>
+      </div>
+    </li>
+  );
 }
 
 
