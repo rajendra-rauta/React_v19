440a15a01881f5d7c32027e360f0b57f26c92402
Rajendra <rajendrarauta979@gmail.com>
2024-12-04 20:04:45 +0530

    another method git push

--- src/hooks/state.jsx
+++ src/hooks/state.jsx
@@ -2,26 +2,41 @@
 import { useState } from "react";
 
 export const State = () => {
-  //   let value = 0;
-  //   const handleButtonClick = () => {
-  //     value++;
-  //     console.log(value);
-  //   };
-  //   let array = useState();
-  //   console.log(array);
-  const [count, setCount] = useState(0);
-  console.log("Parent Component rendered");
+
+
+  let value = 0;
   const handleButtonClick = () => {
-    setCount(() => count + 1);
+    value++;
+    console.log(value);
   };
+  let array = useState();
+  console.log(array);
+
+
+  // const [count, setCount] = useState(0);
+  // console.log("Parent Component rendered");
+  // const handleButtonClick = () => {
+  //   setCount(() => count + 1);
+  // };
   return (
+    // <>
+
+    //   <div className="main-div">
+    //   <h1>{count}</h1>
+    //     <button onClick={handleButtonClick}>Increment</button>
+    //   </div>
+    //   <ChildComponent count={count} />
+    // </>
+
+
+
     <>
-      
+
       <div className="main-div">
-      <h1>{count}</h1>
+        <h1>{value}</h1>
         <button onClick={handleButtonClick}>Increment</button>
       </div>
-      <ChildComponent count={count} />
+      <ChildComponent count={value} />
     </>
   );
 };
