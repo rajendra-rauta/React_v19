b3d05dd4909f1603d47e9845557f5f0e15c7753a
Rajendra <rajendrarauta979@gmail.com>
2024-12-14 21:26:54 +0530

    component

--- src/projects/Todo/Todo.jsx
+++ src/projects/Todo/Todo.jsx
@@ -3,27 +3,26 @@ import { useEffect, useState } from "react";
 import { IoMdCheckmark } from "react-icons/io";
 import { MdDeleteForever } from "react-icons/md";
 import "./Todo.css";
+import { TodoForm } from "./TodoForm";
 export const Todo = () => {
-    const [inputValue, setInputValue] = useState("");
+
     const [task, setTask] = useState([]);
     const [dateTime, setdateTime] = useState("")
+    const handleFormSubmit = (inputValue) => {
 
-    const handleInputChange = (value) => {
-        setInputValue(value)
-    };
-    const handleFormSubmit = (event) => {
-        event.preventDefault();
         if (!inputValue)
             return;
 
         if (task.includes(inputValue)) {
-            setInputValue("");
+            // setInputValue("");
             return;
         }
         setTask((prevTask) => [...prevTask, inputValue])
-        setInputValue("");
-    }
+        // setInputValue("");
+   
+};
     
+ 
     //  date and time 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -35,47 +34,35 @@ export const Todo = () => {
         }, 1000);
 
         return () => clearInterval(interval);
-     }, []);
+    }, []);
 
 
-     // todo delet function 
+    // todo delet function 
 
-     const handleDeleteTodo = (value) =>{
+    const handleDeleteTodo = (value) => {
         console.log(task);
         console.log(value);
-        const updatedTask= task.filter((curTask)=> curTask != value);
+        const updatedTask = task.filter((curTask) => curTask != value);
         setTask(updatedTask);
-        
-     }
+
+    }
 
 
-     // todo handlecleartodo functianility
+    // todo handlecleartodo functianility
 
-     const handleClearTododata =() =>{
+    const handleClearTododata = () => {
         setTask([]);
-     }
+    }
 
-return (
-    <section className="todo-container">
-        <header>
-            <h1>
-                Todo List
-            </h1>
-            <h2 className="date-time">{dateTime}</h2>
-        </header>
-        <section>
-            <form className="form" onSubmit={handleFormSubmit}>
-                <div>
-                    <input type="text" className="todo-input" autoComplete="off"
-                        value={inputValue}
-                        onChange={(event) => handleInputChange(event.target.value)}
-                    >
-                    </input>
-                </div>
-                <div>
-                    <button type="submit" className="todo-btn">Add Task</button>
-                </div>
-            </form>
+    return (
+        <section className="todo-container">
+            <header>
+                <h1>
+                    Todo List
+                </h1>
+                <h2 className="date-time">{dateTime}</h2>
+            </header>
+           <TodoForm onAddTodo = {handleFormSubmit} />
             <section className="myOnOrdList">
                 <ul>
                     {
@@ -84,18 +71,18 @@ return (
                                 <span>{curTask}</span>
                                 <button className="check-btn" ><IoMdCheckmark /></button>
                                 <button className="delete-btn"
-                                 onClick={()=>handleDeleteTodo(curTask)}
-                                 >
-                                 <MdDeleteForever /></button>
+                                    onClick={() => handleDeleteTodo(curTask)}
+                                >
+                                    <MdDeleteForever /></button>
                             </li>
                         })
                     }
                 </ul>
             </section>
             <section className="clear-btn" onClick={handleClearTododata}>Clear all</section>
-        </section>
 
-    </section>
-)
+
+        </section>
+    )
 
 }
